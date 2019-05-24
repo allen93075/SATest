012222038cacc2620b02a343c8d7880acf79d8fa
李少軒 <allen93075@gmail.com>
2019-05-24 19:24:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/satest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.satest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Username_input;
+    private EditText Passward_input;
+    private Button Signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Username_input = (EditText) findViewById(R.id.Username_input);
+        Passward_input = (EditText) findViewById(R.id.Password_input);
+        Signup = (Button) findViewById(R.id.Signup);
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this  , SignUp.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
