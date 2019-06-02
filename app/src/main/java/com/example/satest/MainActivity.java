b248539b630424401d57c6ea241c7969b7fa87fd
李少軒 <allen93075@gmail.com>
2019-06-02 16:11:55 +0800

    API問題

--- app/src/main/java/com/example/satest/MainActivity.java
+++ app/src/main/java/com/example/satest/MainActivity.java
@@ -6,8 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.satest.Fragment.Front;
+import com.example.satest.Retrofit.Api;
+import com.example.satest.Retrofit.RetrofitManager;
+import com.example.satest.Retrofit.User_data;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,14 +35,17 @@ public class MainActivity extends AppCompatActivity {
         Username_input = (EditText) findViewById(R.id.Username_input);
         Passward_input = (EditText) findViewById(R.id.Password_input);
         Signup = (Button) findViewById(R.id.Signup);
-        Signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, SignUp.class);
-                startActivity(intent);
-            }
-        });
+
+
+//        Signup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, SignUp.class);
+//                startActivity(intent);
+//            }
+//        });
+
 
         Login = (Button) findViewById(R.id.Login);
         Login.setOnClickListener(new View.OnClickListener() {
@@ -44,8 +57,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //用Sign_UP button 測試
+        Button info = findViewById(R.id.Signup);
+        final TextView tv3 = findViewById(R.id.tv3);
+        final StringBuffer sb = new StringBuffer();
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //從server撈資料回來處理
+                Api postApi = RetrofitManager.getClient().create(Api.class);
+                postApi.user().enqueue(new Callback<List<User_data>>() {
+                    @Override
+                    public void onResponse(Call<List<User_data>> call, Response<List<User_data>> response) {
 
+                        List<User_data> list = response.body();
+//                        for (User_data p : list) {
+//                            sb.append("OOOAAAAOOO");
+//                            sb.append(list);
+//                            sb.append("\n");
+//                            sb.append("---------------------\n");
+//                        }
+                        sb.append(list);
+                        tv3.setText(sb);
+                    }
 
+                    @Override
+                    public void onFailure(Call<List<User_data>> call, Throwable t) {
+                        tv3.setText("錯誤");
+
+                    }
+                });
+
+                //tv3.setText(sb.toString()+"到底有沒有呢????");
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/satest/Retrofit/RetrofitManager.java
+++ app/src/main/java/com/example/satest/Retrofit/RetrofitManager.java
@@ -1,12 +1,14 @@
 package com.example.satest.Retrofit;
 
-import android.graphics.Bitmap;
 
 import okhttp3.OkHttpClient;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitManager {
+
+
 
     private static RetrofitManager manager = new RetrofitManager();
     private Retrofit retrofit;
