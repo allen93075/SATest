f531e249de62f04c9a374a4c1f47d0af54e1b30a
李少軒 <allen93075@gmail.com>
2019-06-05 19:59:44 +0800

    API測試

--- app/src/main/java/com/example/satest/Retrofit/Config.java
+++ app/src/main/java/com/example/satest/Retrofit/Config.java
@@ -2,7 +2,7 @@ package com.example.satest.Retrofit;
 
 public class Config {
 
-    public static final String URL = "https://api.airtable.com/v0/appkJPE8QWQLB17KW/User?api_key=keycL5kGwKuZlYMdn";
+    public static final String URL = "https://api.airtable.com/v0/appkJPE8QWQLB17KW/";
 
 }
 
