dfb9c06a5511019f66d6f45a2f26918214faada5
406402608 <406402608@gapp.fju.edu.tw>
2019-06-15 15:53:07 +0800

    本地選擇圖片

--- app/src/main/java/com/example/satest/Fragment/upload_fragment.java
+++ app/src/main/java/com/example/satest/Fragment/upload_fragment.java
@@ -1,5 +1,6 @@
 package com.example.satest.Fragment;
 
+import android.net.sip.SipSession;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,16 +8,65 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import java.io.FileNotFoundException;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import com.example.satest.R;
+import static android.app.Activity.RESULT_OK;
 
-public class upload_fragment extends Fragment {
-    @Nullable
+public class upload_fragment extends Fragment{
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.upload_fragment, container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.upload_fragment, container, false);
+
+        Button button = (Button) view.findViewById(R.id.select_image);
+        button.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                //開啟Pictures畫面Type設定為image
+                intent.setType("image/*");
+                //使用Intent.ACTION_GET_CONTENT這個Action                                            //會開啟選取圖檔視窗讓您選取手機內圖檔
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                //取得相片後返回本畫面
+                startActivityForResult(intent, 1);
+            }
+
+        });
+        return view;
     }
 
-
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //當使用者按下確定後
+        if (resultCode == RESULT_OK) {
+            //取得圖檔的路徑位置
+            Uri uri = data.getData();
+            //寫log
+            Log.e("uri", uri.toString());
+            //抽象資料的接口
+            ContentResolver cr = this.getActivity().getContentResolver();
+            try {
+                //由抽象資料接口轉換圖檔路徑為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                //取得圖片控制項ImageView
+                ImageView imageView = (ImageView) getView().findViewById(R.id.upload_image);
+                // 將Bitmap設定到ImageView
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(),e);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
