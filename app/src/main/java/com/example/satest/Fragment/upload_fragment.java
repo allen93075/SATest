ebdd70b5540ba80fadfda798e8a810c4b123a11b
406402608 <406402608@gapp.fju.edu.tw>
2019-06-17 04:53:41 +0800

    圖片及敘述上傳完成

--- app/src/main/java/com/example/satest/Fragment/upload_fragment.java
+++ app/src/main/java/com/example/satest/Fragment/upload_fragment.java
@@ -8,7 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -22,16 +27,49 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import com.example.satest.R;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import static android.app.Activity.RESULT_OK;
 
 public class upload_fragment extends Fragment{
+
+    public FirebaseStorage storage = FirebaseStorage.getInstance();
+    public ImageView imageView;
+    public Bitmap bitmap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.upload_fragment, container, false);
 
-        Button button = (Button) view.findViewById(R.id.select_image);
-        button.setOnClickListener(new Button.OnClickListener(){
+        Button selectButton = (Button) view.findViewById(R.id.select_image);
+        Button uploadButton = (Button) view.findViewById(R.id.upload_ready);
+        final EditText textInput = (EditText) view.findViewById(R.id.description);
+
+        uploadButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setDrawingCacheEnabled(true);
+                imageView.buildDrawingCache();
+                bitmap = imageView.getDrawingCache();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                imageView.setDrawingCacheEnabled(true);
+                byte[] data = baos.toByteArray();
+
+                String path = UUID.randomUUID() + ".png";
+                StorageReference firebaseRdf = storage.getReference(path);
+                StorageMetadata metadata = new StorageMetadata.Builder()
+                        .setCustomMetadata("description", textInput.getText().toString()).build();
+
+                UploadTask uploadTask = firebaseRdf.putBytes(data, metadata);
+            }
+        });
+
+        selectButton.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v) {
                 Intent intent = new Intent();
                 //開啟Pictures畫面Type設定為image
@@ -43,6 +81,7 @@ public class upload_fragment extends Fragment{
             }
 
         });
+
         return view;
     }
 
@@ -58,9 +97,9 @@ public class upload_fragment extends Fragment{
             ContentResolver cr = this.getActivity().getContentResolver();
             try {
                 //由抽象資料接口轉換圖檔路徑為Bitmap
-                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
                 //取得圖片控制項ImageView
-                ImageView imageView = (ImageView) getView().findViewById(R.id.upload_image);
+                imageView = (ImageView) getView().findViewById(R.id.upload_image);
                 // 將Bitmap設定到ImageView
                 imageView.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
