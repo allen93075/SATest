2e6ee6c578d68c681818b55712fc9e4b64b32dd9
李少軒 <allen93075@gmail.com>
2019-06-16 13:28:46 +0800

    6/16

---
+++ app/src/main/java/com/example/satest/Intro_upload.java
@@ -0,0 +1,15 @@
+package com.example.satest;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Intro_upload extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upload_intro);
+
+
+    }
+}
